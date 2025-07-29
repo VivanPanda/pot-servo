e18f8ba6f7691d8fcbce09b982b4553a4d183498
VivanPanda <vivanpanda13@gmail.com>
2025-07-29 18:13:20 +0400

    add initial code

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,23 @@
+int potPin = A7;
+int servoPin = 7;
+int potVal;
+float servoAngle;
+
+#include <Servo.h>
+
+Servo myservo;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(potPin, INPUT);
+  Serial.begin(9600);
+  myservo.attach(servoPin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  potVal = analogRead(potPin);
+  Serial.println(potVal);
+  servoAngle = ((-120./340.) * potVal) + 180;
+  myservo.write(servoAngle);
 }
